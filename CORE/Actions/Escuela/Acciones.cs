5a6f52f76fa7206b15be9e06fc3e5d656d31c960
Mona_Lisa <monalisa@davinci.com>
2022-03-22 10:19:15 +0100

    Update Actions

---
+++ CORE/Actions/Escuela/Acciones.cs
@@ -0,0 +1,19 @@
+public class Acciones
+{
+    public void Crear()
+    {
+
+    }
+    public void Editar()
+    {
+
+    }
+    public void Listar()
+    {
+
+    }
+    public void Borrar()
+    {
+        
+    }
+}
